f3a18916f624309688ca236fa442ba2d67daa6cb
Fritz <fritz.developer10@gmail.com>
2023-04-02 20:38:52 -0500

    objetos

--- scripts/65-retornar-valores-booleanos.js
+++ scripts/65-retornar-valores-booleanos.js
@@ -2,7 +2,7 @@
 //RETORNAR VALORES BOOLEANOS
 
 /* 
-Esta funcion if else, quse muestra a continuación 
+Esta funcion if else, que muestra a continuación 
 se puede presentar de una manera mas simplificada
 
 function esMenorQue(a,b) {
@@ -19,4 +19,4 @@ function esMenorQue(a,b){
 return a < b;
 }
 
-console.log(esMenorQue(3,6));
+console.log(esMenorQue(3,6)); // true
